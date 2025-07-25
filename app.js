df47bdcb4577e4f778da3fca34ad1626dc7b5e66
Aakash <akkiaakash09@gmail.com>
2025-07-25 21:53:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-const API_KEY = "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";
+const API_KEY = process.env.API_KEY;
 
 app.get("/", (req, res) => {
     res.render("index.ejs");
